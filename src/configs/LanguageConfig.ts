3980d6ff3180cf69604649e051de56a6f56597af
KhanhCan2k4 <levietkhanh2k4@gmail.com>
2024-09-22 18:54:58 +0700

    setup multilanguage, test multilanguage

---
+++ src/configs/LanguageConfig.ts
@@ -0,0 +1,23 @@
+import { createContext } from "react";
+import languages from "../../languages.json";
+
+export enum Languages {
+    "VN",
+    "EN",
+    "JA"
+}
+
+export type LanguageType = {
+    VIEW_ALL: string;
+    SHOWING_PAGE: string;
+}
+
+type LanguageConfigType = {
+    language: LanguageType,
+    setLanguage: (language: Languages) => void,
+}
+
+export const LanguageContext = createContext<LanguageConfigType>({
+    language: languages.VN,
+    setLanguage: () => { }
+});
