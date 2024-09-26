8e306d14886973e80a732de882c60a0c0f8cec36
KhanhCan2k4 <levietkhanh2k4@gmail.com>
2024-09-26 11:30:13 +0700

    craete screen in nav, nav bar

---
+++ src/views/screens/Chat.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+import { Text, View } from "react-native";
+
+export default function ChatScreen() {
+  return (
+    <View>
+      <Text>Chat Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
