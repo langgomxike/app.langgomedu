78c0141d96d000fd01b8932958130a743579f232
VanDuPlus <22211TT1357@mail.tdc.edu.vn>
2024-09-30 16:39:59 +0700

    fixed icon components, login screen, register, otp, change password

--- src/views/screens/ChangePassword.tsx
+++ src/views/screens/ChangePassword.tsx
@@ -12,48 +12,53 @@ export default function DuTestScreen() {
       <View style={styles.icon}>
         <MyIcon icon={AppIcon.ic_back_circle} />
       </View>
-      <Text style={styles.title}>Thay đổi mật khẩu</Text>
-      <Text style={styles.content}>Mật khẩu phải tối thiểu 6 kí tự</Text>
-      <View style={styles.test}>
-        <View style={styles.input}>
-          <InputRegister
-            label="Nhập mật khẩu hiện tại"
-            required={true}
-            placeholder="Nhập mật khẩu hiện tại"
-            type="password"
-            iconName="password"
-            onChangeText={myEmptyFunction}
-          />
-        </View>
-        <View style={styles.input}>
-          <InputRegister
-            label="Nhập mật khẩu mới"
-            required={true}
-            placeholder="Nhập mật khẩu mới"
-            type="password"
-            iconName="password"
-            onChangeText={myEmptyFunction}
-          />
-        </View>
-        <View style={styles.input}>
-          <InputRegister
-            label="Xác nhận lại mật khẩu"
-            required={true}
-            placeholder="Xác nhận lại mật khẩu"
-            type="password"
-            iconName="password"
-            onChangeText={myEmptyFunction}
-          />
+      <View style={styles.row}>
+        <View>
+      <Text style={styles.title}> Đổi mật khẩu</Text>
+      <Text style={styles.content}>Hãy ghi nhớ mật khẩu của bạn</Text>
+      </View>
+      <View></View>
+      </View>
+     
+      <View style={styles.input}>
+        <InputRegister
+          label="Mật khẩu hiện tại của bạn"
+          required={true}
+          onChangeText={myEmptyFunction}
+          placeholder="Mật khẩu hiện tại của bạn"
+          type="password"
+          iconName="password"
+        ></InputRegister>
+      </View>
+      <View style={styles.input}>
+        <InputRegister
+          label="Mật khẩu mới của bạn"
+          required={true}
+          onChangeText={myEmptyFunction}
+          placeholder="Mật khẩu mới của bạn"
+          type="password"
+          iconName="password"
+        ></InputRegister>
+      </View>
+      <View style={styles.input}>
+        <InputRegister
+          label="Xác nhận mật khẩu mới của bạn"
+          required={true}
+          onChangeText={myEmptyFunction}
+          placeholder="Xác nhận mật khẩu mới của bạn"
+          type="password"
+          iconName="password"
+        ></InputRegister>
+      </View>
+      <View style={styles.row1}>
+        <Text></Text>
+        <View style={styles.testQuenMatKhau}>
+        <Text>Mật khẩu phải từ 6 đến 24 kí tự</Text>
         </View>
       </View>
+      <View style={styles.btn}>
+      <Button  title="Xác nhận" textColor="white" backgroundColor="blue" onPress={myEmptyFunction}></Button>
 
-      <View style={styles.button}>
-        <Button
-          title="Xác nhận thay đổi"
-          textColor="white"
-          backgroundColor="blue"
-          onPress={myEmptyFunction}
-        />
       </View>
     </View>
   );
@@ -65,44 +70,59 @@ const styles = StyleSheet.create({
     // justifyContent: "center",
   },
   icon: {
-    top: 30,
-    left: '-45%',
+    marginTop: "12%",
+    marginLeft: "-85%",
   },
-
+  iconInput: {
+    left: 10,
+    top: 25,
+    justifyContent: "center",
+  },
   input: {
-    marginBottom: "7%",
-    width: 380,
+    top: "0%",
+    height: 20,
+    width: "90%",
+    marginBottom: "20%",
   },
+  
   img: {
     top: -10,
-    width: 200,
-    height: 200,
+    width: 250,
+    height: 250,
     alignItems: "center",
   },
   title: {
-    marginTop: "10%",
     fontSize: 20,
-    left: "-25%",
-    fontWeight: "bold",
+    fontWeight:'bold',
   },
   content: {
     fontSize: 14,
-    marginBottom: 40,
-    left: "-22%",
+    marginBottom: "20%",
   },
   test: {
+    top: -10,
     // height: 50,
     // backgroundColor: "green",
   },
-  luuy: {
+  testQuenMatKhau: {
     top: -10,
-    left: 50,
+    left: 80,
+    marginBottom: "20%",
   },
-  button: {
-    top: "35%",
-    alignItems: "center",
+
+  dangky: {},
+  row1: {
+    flexDirection: "row", // Đặt các biểu tượng nằm trên cùng một hàng
+    justifyContent: "space-evenly", // Cân đối khoảng cách giữa các biểu tượng
+    marginBottom: 20, // Thêm khoảng cách dưới hàng icon
   },
-  dangky: {
-    top: 120,
+  row: {
+     // Đặt các biểu tượng nằm trên cùng một hàng
+    marginLeft:'-40%', // Cân đối khoảng cách giữa các biểu tượng
+    marginBottom:' -12%', // Thêm khoảng cách dưới hàng icon
   },
+  btn:{
+    marginTop:'30%',
+
+  }
 });
