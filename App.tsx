ee0d42fefc2aff3f7a0139898b203b41b9c1fb8c
Van-Hoang-Dev <vanhoang24edu@gmail.com>
2024-10-15 11:34:36 +0700

    edit .env.exmaple

--- App.tsx
+++ App.tsx
@@ -23,8 +23,6 @@ import Register1Screen from "./src/views/screens/Register1";
 import Register2Screen from "./src/views/screens/Register2";
 import OTPScreen from "./src/views/screens/OTP";
 import ChangePasswordScreen from "./src/views/screens/ChangePassword";
-import AdminHome from "./src/views/screens/Admin/AdminHome";
-import GeneralManagement from "./src/views/screens/Admin/GeneralManagement";
 const Stack = createNativeStackNavigator();
 const SCREEN_PADDING_TOP = 50;
 const SCREEN_PADDING_HORIZONTAL = 0;
@@ -101,11 +99,13 @@ export default function App() {
           name={ScreenName.LOGIN} component={LoginScreen}/>
         </Stack.Navigator>
       </NavigationContainer>
+
       {/* <LeanerAttendance></LeanerAttendance> */}
       {/* <TutorAttendance></TutorAttendance> */}
       {/* <LeanerAttendance></LeanerAttendance> */}
       {/* <History/> */}
       {/* <HoangTestScreen/> */}
+
       </GestureHandlerRootView>
     </LanguageContext.Provider>
   );
