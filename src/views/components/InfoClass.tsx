7d1556e13845db5d7ae5869dfb81c7d8f032eff1
phuongnhi130920@gmail.com <phuongnhi130920@gmail.com>
2024-10-28 16:04:55 +0700

    viet giao dien tao lop, fetch data classlevel, subject

--- src/views/components/InfoClass.tsx
+++ src/views/components/InfoClass.tsx
@@ -1,44 +1,172 @@
-import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 import CustomInput from "./Inputs/CustomInput";
+import { Picker } from "@react-native-picker/picker";
+import AClassLevel from "../../apis/AClassLevel";
+import ClassLevel from "../../models/ClassLevel";
+import Major from "../../models/Major";
+import AMajor from "../../apis/AMajor";
 
 const InfoClass = () => {
   const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [capHocList, setCapHocList] = useState<ClassLevel[]>([]); // đặt select
+  const [selectedCapHoc, setSelectedCapHoc] = useState("");
 
+  // useState MÔN HỌC, FETCH DATA
+  const [monHoc, setMonHoc] = useState<number | string>(""); // đặt select
+  const [isOtherSelected, setIsOtherSelected] = useState(false); // Kiểm tra khi chọn "Khác"
+  const [customInput, setCustomInput] = useState(""); // Giá trị nhập khi chọn "Khác"
+  const [isLoading, setIsLoading] = useState(false);
+  const [pickerItems, setPickerItems] = useState<
+    { label: string; value: number | "other" }[]
+  >([{ label: "Khác", value: "other" }]); // Danh sách các buổi học
+
+  // useEffect
+  useEffect(() => {
+    AMajor.getAllMajors((majors) => {
+      const majorItems = majors.map((major) => ({
+        label: major.vn_name,
+        value: major.id,
+      }));
+      setPickerItems((prevItems) => {
+        const existingIds = new Set(prevItems.map((item) => item.value));
+        const uniqueMajorItems = majorItems.filter(
+          (item) => !existingIds.has(item.value)
+        );
+        return [...uniqueMajorItems, ...prevItems];
+      });
+    }, setIsLoading);
+  }, []);
+
+  const handleSelectChange = (itemValue: any | "other") => {
+    if (itemValue === "other") {
+      setIsOtherSelected(true); // Chuyển Picker thành TextInput nếu chọn "Khác"
+    } else {
+      setMonHoc(itemValue); // Cập nhật giá trị đã chọn từ Picker
+      setIsOtherSelected(false); // Ẩn TextInput nếu chọn lại từ các mục khác
+    }
+  };
+
+  const handleAddCustomValue = () => {
+    if (customInput.trim() !== "") {
+      const newItem = {
+        label: customInput,
+        value: Date.now(), // Giả sử đặt tạm 0 cho value nếu cần
+      };
+      setPickerItems((prevItems) => {
+        const otherItem = prevItems.find((item) => item.value === "other");
+        const itemsWithoutOther = prevItems.filter(
+          (item) => item.value !== "other"
+        );
+        return otherItem
+          ? [newItem, ...itemsWithoutOther, otherItem] // Thêm giá trị mới vào trước mục "Khác"
+          : [newItem, ...itemsWithoutOther]; // Trường hợp "Khác" không tồn tại
+      });
+      setMonHoc(newItem.value); // Cập nhật giá trị được chọn là giá trị mới nhập
+      setCustomInput(""); // Reset lại ô nhập
+      setIsOtherSelected(false); // Quay lại Picker sau khi nhập xong
+    }
+  };
+
+  /** ========================================= */
+
+  /**
+   * FETCH DATA CẤP HỌC
+   */
+
+  useEffect(() => {
+    AClassLevel.getAllClassLevels((classLevels) => {
+      setCapHocList(classLevels);
+      // console.log("hihi ", classLevels);
+    });
+  }, []);
+
+  /**
+   * ===========================================
+   */
   return (
-    <View style={{ margin: 10 }}>
-      <CustomInput
-        placeholder="Nhập tiêu đề..."
-        type="text"
-        label="Tiêu đề"
-        onChangeText={setTitle}
-        required
-      />
-      <CustomInput
-        placeholder="Nhập mô tả..."
-        type="textarea"
-        label="Mô tả"
-        onChangeText={setTitle}
-        required
-      />
-      <View style={styles.container}>
-        <View style={[styles.inputContainer, { flex: 8 }]}>
-          <CustomInput
-            placeholder="Nhập môn học..."
-            type="text"
-            label="Chọn môn học cho lớp"
-            onChangeText={setTitle}
-            required
-          />
+    <View style={styles.container}>
+      <View style={styles.marginInput}>
+        <CustomInput
+          placeholder="Nhập tiêu đề..."
+          type="text"
+          label="Tiêu đề"
+          onChangeText={setTitle}
+          required
+          value={title}
+        />
+      </View>
+      <View style={styles.marginInput}>
+        <CustomInput
+          placeholder="Nhập mô tả..."
+          type="textarea"
+          label="Mô tả"
+          onChangeText={setDesc}
+          required
+          value={desc}
+        />
+      </View>
+      <View style={[styles.containerRow, styles.marginInput]}>
+        <View style={[styles.inputContainer, { flex: 6 }]}>
+          <Text style={styles.label}>
+            Chọn môn học cho lớp <Text style={styles.required}>*</Text>
+          </Text>
+
+          {isLoading ? (
+            <Text>Đang tải...</Text>
+          ) : isOtherSelected ? (
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={styles.textInput}
+                placeholder="Nhập môn học..."
+                value={customInput}
+                onChangeText={setCustomInput}
+              />
+              <Button title="Thêm" onPress={handleAddCustomValue} />
+            </View>
+          ) : (
+            <View style={styles.pickerContainer}>
+              <Picker
+                selectedValue={monHoc}
+                onValueChange={(itemValue) => handleSelectChange(itemValue)}
+                style={styles.picker}
+              >
+                {pickerItems.map((item) => (
+                  <Picker.Item
+                    label={item.label}
+                    value={item.value}
+                    key={item.value}
+                  />
+                ))}
+              </Picker>
+            </View>
+          )}
         </View>
-        <View style={[styles.inputContainer, { flex: 4 }]}>
-          <CustomInput
-            placeholder="Cấp học..."
-            type="text"
-            label="Cấp học"
-            onChangeText={setTitle}
-            required
-          />
+
+        {/* CẤP HOC */}
+        <View style={[styles.inputContainer, { flex: 6 }]}>
+          <Text style={styles.label}>
+            Cấp học <Text style={styles.required}>*</Text>
+          </Text>
+          <View style={styles.pickerContainer}>
+            <Picker
+              selectedValue={selectedCapHoc}
+              onValueChange={(itemValue) => setSelectedCapHoc(itemValue)}
+              style={styles.picker}
+            >
+              {/* <Picker.Item label="Sơ cấp" value="0" />
+              <Picker.Item label="Trung cấp" value="1" />
+              <Picker.Item label="Cao cấp" value="2" /> */}
+              {capHocList.map((classLevel) => (
+                <Picker.Item
+                  label={classLevel.vn_name}
+                  value={classLevel.id}
+                  key={classLevel.id}
+                />
+              ))}
+            </Picker>
+          </View>
         </View>
       </View>
     </View>
@@ -47,7 +175,17 @@ const InfoClass = () => {
 
 const styles = StyleSheet.create({
   container: {
+    padding: 20,
+    backgroundColor: "#FFFF",
+  },
+  containerRow: {
     flexDirection: "row",
+  },
+  pickerContainer: {
+    borderWidth: 1,
+    borderColor: "#ddd",
+    borderRadius: 10,
+    backgroundColor: "#fff",
   },
   row: {
     flexDirection: "row",
@@ -55,6 +193,36 @@ const styles = StyleSheet.create({
   },
   inputContainer: {
     marginRight: 10, // khoảng cách giữa 2 input
+  },
+  marginInput: {
+    marginBottom: 20,
+  },
+  label: {
+    fontWeight: "bold",
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  required: {
+    color: "red",
+  },
+  picker: {
+    height: 50,
+    width: 150,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginTop: 10,
+    paddingHorizontal: 10,
+  },
+  textInput: {
+    flex: 1,
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginRight: 10,
+    paddingHorizontal: 10,
   },
 });
 
