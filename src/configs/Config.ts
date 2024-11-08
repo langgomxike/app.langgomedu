80c8178bfb2c606cc0f5d0b9867e6f1b24ad830a
khanhcan2k4 <levietkhanh2k4@gmail.com>
2024-11-08 11:57:20 +0700

    add before merge

--- src/configs/Config.ts
+++ src/configs/Config.ts
@@ -1,3 +1,3 @@
 export default class Config {
-    static API_BASE_URL = "https://068d-202-12-95-109.ngrok-free.app/api";
+    static API_BASE_URL = "https://add6-202-12-95-109.ngrok-free.app/api";
 }
