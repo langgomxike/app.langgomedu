b0b00be636c70aa24cb4867faad705bad3d88012
khanhcan2k4 <levietkhanh2k4@gmail.com>
2024-11-11 21:01:43 +0700

    commit before revert

--- src/configs/Config.ts
+++ src/configs/Config.ts
@@ -1,7 +1,3 @@
 export default class Config {
-<<<<<<< HEAD
-    static API_BASE_URL = "http://192.168.43.156:3002/api";
-=======
     static API_BASE_URL = "https://add6-202-12-95-109.ngrok-free.app/api";
->>>>>>> parent of f2e8517 (commit before merge dev)
 }
