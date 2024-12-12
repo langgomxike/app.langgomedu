ebd0bf891d5a885fc5038986b5fa3b0b96e99a1c
khanhcan2k4 <levietkhanh2k4@gmail.com>
2024-12-12 16:06:29 +0700

    fix mark as read message

--- src/configs/ConfigUrl.ts
+++ src/configs/ConfigUrl.ts
@@ -1,7 +1,7 @@
 // Your ip address: 192.168.43.156
 const ReactAppUrl = {
-  API_BASE_URL: "http://192.168.1.154:3003/api",
-  PUBLIC_URL: "http://192.168.1.154:3003",
+  API_BASE_URL: "http://192.168.107.121:3003/api",
+  PUBLIC_URL: "http://192.168.107.121:3003",
 };
 
 export default ReactAppUrl;
