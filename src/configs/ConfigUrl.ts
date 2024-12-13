406ce0c18ffcb190ffd6a3fdad8354f93dfac1df
khanhcan2k4 <levietkhanh2k4@gmail.com>
2024-12-13 08:25:17 +0700

    add before pull dev

--- src/configs/ConfigUrl.ts
+++ src/configs/ConfigUrl.ts
@@ -1,7 +1,7 @@
 // Your ip address: 192.168.43.156
 const ReactAppUrl = {
-  API_BASE_URL: "https://b081-103-249-22-51.ngrok-free.app/api",
-  PUBLIC_URL: "https://b081-103-249-22-51.ngrok-free.app",
+  API_BASE_URL: "https://22d7-103-249-22-51.ngrok-free.app/api",
+  PUBLIC_URL: "https://22d7-103-249-22-51.ngrok-free.app",
 };
 
 export default ReactAppUrl;
